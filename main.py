353168fda6edeaff5c1a9c6b23437c78184b69d5
JYYYeung <sheepyy039@gmail.com>
2024-02-13 00:57:24 +0800

    rename metatube.py to main.py

--- main.py
+++ main.py
@@ -3,17 +3,18 @@
 # coding: utf-8
 import os
 
+from flask import Flask
 from gevent import get_hub
 from str2bool import str2bool
 
 from metatube import create_app, socketio
 
 if __name__ == "__main__":
-    app = create_app()
-    port = os.environ.get("PORT", 35000)
-    host = os.environ.get("HOST", "0.0.0.0")
-    debug = os.environ.get("DEBUG", False)
-    log_output = os.environ.get("LOG", False)
+    app: Flask = create_app()
+    port: str | int = os.environ.get("PORT", 35000)
+    host: str = os.environ.get("HOST", "127.0.0.1")
+    debug: str | bool = os.environ.get("DEBUG", True)
+    log_output: str | bool = os.environ.get("LOG", False)
     get_hub().NOT_ERROR += (KeyboardInterrupt,)
     try:
         print(f"Starting the webserver on http://{host}:{port}...")
